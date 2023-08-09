e1988d7eb907a2463729b7d687f2d2621efb7d60
Rakesh Kumar Tunga <rt21aah@herts.ac.uk>
2023-08-09 13:28:53 +0100

    ansible sample file

--- main.tf
+++ main.tf
@@ -9,10 +9,10 @@ variable "params" {
   default = [
     #{name = "demo1", value = "demo1", type = "String"},
     #{name = "demo2", value = "demo2", type = "String"}
-    { name = "roboshop-dev.frontend.catalogue_url", value = "http://catalogue-dev.pandudevopsb72.store", type="String"},
-    { name = "roboshop-dev.frontend.user_url", value = "http://catalogue-dev.pandudevopsb72.store", type="String"},
-    { name = "roboshop-dev.frontend.cart_url", value = "http://catalogue-dev.pandudevopsb72.store", type="String"},
-    { name = "roboshop-dev.frontend.payment_url", value = "http://catalogue-dev.pandudevopsb72.store", type="String"},
-    { name = "roboshop-dev.frontend.shipping_url", value = "http://catalogue-dev.pandudevopsb72.store", type="String"}
+    { name = "roboshop-dev.frontend.catalogue_url", value = "http://catalogue-dev.pandudevopsb72.store:8080/", type="String"},
+    { name = "roboshop-dev.frontend.user_url", value = "http://catalogue-dev.pandudevopsb72.store:8080/", type="String"},
+    { name = "roboshop-dev.frontend.cart_url", value = "http://catalogue-dev.pandudevopsb72.store:8080/", type="String"},
+    { name = "roboshop-dev.frontend.payment_url", value = "http://catalogue-dev.pandudevopsb72.store:8080/", type="String"},
+    { name = "roboshop-dev.frontend.shipping_url", value = "http://catalogue-dev.pandudevopsb72.store:8080/", type="String"}
   ]
 }
